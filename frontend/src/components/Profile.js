af5f830144438d104fc24c12226a6da4b02769fc
David Marko <88544099+david-marko@users.noreply.github.com>
2023-01-07 09:53:38 +0000

    chore: remove console.log

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -129,7 +129,7 @@ class Profile extends React.Component {
           <div className="row p-4 text-center">
             <div className="user-info col-xs-12 col-md-8 offset-md-2">
               <img
-                src={profile.image !== null ? profile.image : 'placeholder.png' }
+                src={profile.image !== null ? profile.image : '/placeholder.png' }
                 className="user-img"
                 alt={profile.username}
               />
